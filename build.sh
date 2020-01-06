e927eff8bcb0f4a1a358e5f6ad9ea74e8726d466
m9-unix <mundusnine@gmail.com>
2020-01-06 18:02:37 -0500

    We can compile with gcc/clang and run haxe; We now need to find out how to compile with wasienv(initial tests showed errors on compilation with haxe external libs i.e. pcre and zlib).

--- build.sh
+++ build.sh
@@ -1,2 +1,59 @@
-#!/bin/bash 
-gcc -I/home/jsnadeau/.opam/default/lib/ocaml /home/jsnadeau/haxe/libs/extc/process_stubs.c /home/jsnadeau/haxe/libs/extc/extc_stubs.c -L/home/jsnadeau/.opam/default/lib/ocaml -L/home/jsnadeau/haxe/libs /home/jsnadeau/haxe/libs/ziplib/ziplib.a /home/jsnadeau/haxe/libs/extc/extc.a /home/jsnadeau/haxe/libs/extlib-leftovers/extlib-leftovers.a /home/jsnadeau/haxe/libs/pcre/pcre.a /home/jsnadeau/haxe/libs/objsize/objsize.a /home/jsnadeau/haxe/libs/javalib/javalib.a haxe.bc.c -o haxe -Wall -lcamlrun -lm -ldl -lncurses -lthreads -lpthread -lz -lpcre
+#!/bin/bash
+# https://github.com/DigitalArsenal/openssl.js/blob/master/packages/openssl/build.sh
+# @:Incomplete
+# We should do something like they did to compile to wasm
+includes="-I$HOME/.opam/default/lib/ocaml -I$HOME/haxe/libs/objsize -I."
+
+linkPaths="-L$HOME/.opam/default/lib/ocaml -L$HOME/haxe/libs -L$HOME/.opam/default/lib/sha -L."
+
+afiles=''
+path="$HOME/haxe/libs"
+ofiles="$path/extc/process_stubs.c $path/pcre/pcre_stubs.c $path/extc/extc_stubs.c $path/objsize/c_objsize.c"
+
+cargs='-lcamlrun -lm -ldl -lncurses -lthreads -lpthread -lpcre -lz -lunix -lcamlstr -lsha1'
+
+outMain='   
+int main (int argc, char ** argv)
+{   
+    caml_startup(argv) ;
+    return 0 ;
+}'
+mainIncludes="#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>"
+
+sed -i -e 's/HAXE_OUTPUT=haxe/HAXE_OUTPUT=haxe.bc.c/g' ./Makefile
+sed -i -e 's/haxe.exe/haxe.bc.c/g' ./Makefile
+
+make
+
+sed -i "1i$mainIncludes" ./haxe.bc.c
+echo "$outMain" >> ./haxe.bc.c
+mv ./haxe.bc.c ./main.c
+
+cur=$PWD
+for f in $(find $HOME/haxe/libs -name "Makefile");
+    do
+    cd ${f%"/Makefile"}
+    make
+done
+cd $cur
+
+for f in $(find $HOME/haxe/libs -name "*.a");
+    do
+    afiles="$afiles $f"
+    
+done
+
+clang $includes main.c $ofiles $afiles $linkPaths -o haxe  $cargs
+echo haxe was built
+
+for f in $(find $HOME/haxe/libs -name "Makefile");
+    do
+    cd ${f%"/Makefile"}
+    make clean
+done
+
+for f in $(find . -maxdepth 2 -name "*.o");
+    do
+    rm $f
+    
+done
