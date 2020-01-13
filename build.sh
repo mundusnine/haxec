538579a5682360be180a589e706838d49555b794
m9-unix <mundusnine@gmail.com>
2020-01-13 13:36:29 -0500

    Found the issues(ocamlrun needs 64 bit pointers but emscripten only offers 32 bit compilation).

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# https://github.com/DigitalArsenal/openssl.js/blob/master/packages/openssl/build.sh
-# @:Incomplete
-# We should do something like they did to compile to wasm
-# emcc -I. -I../ocaml/runtime -I/home/jsnadeau/.opam/default/lib/ocaml sha1_stubs.c -o libsha1.so
+# As of know the compilation works but the ocaml runtime fails because emscripten only compiles to wasm32 and the runtime needs 64 bit pointers
 includes="-I$HOME/haxec/ocaml/otherlibs -I$HOME/haxec/ocaml/runtime -I$HOME/haxec/pcre -I$HOME/haxe/libs/objsize -I. -I$HOME/haxec/sha"
 
-linkPaths="-L$HOME/haxec/ocaml/runtime -L/home/jsnadeau/haxec/pcre/.libs -L$HOME/haxec/ocaml/otherlibs/unix -L$HOME/haxec/ocaml/otherlibs/str -L$HOME/haxec/ocaml/otherlibs/systhreads -L$HOME/haxe/libs -L$HOME/haxec/pcre -L$HOME/haxec/sha -L. -L--no-check-features"
+linkPaths="-L$HOME/haxec/pcre/.libs/libpcre.so -L$HOME/haxec/ocaml/runtime/libcamlrun.a -L$HOME/haxec/ocaml/otherlibs/unix/libunix.so -L$HOME/haxec/ocaml/otherlibs/systhreads/libthreads.so -L$HOME/haxec/sha/libsha1.so -L. -L--no-check-features"
 
 afiles=''
 path="$HOME/haxe/libs"
@@ -14,12 +11,47 @@ ofiles="$HOME/haxec/sha/sha1c.c $path/extc/process_stubs.c $path/pcre/pcre_stubs
 cargs='-lcamlrun_shared -lm -ldl -lthreads -lpthread -lunix -lpcre -lcamstr -lsha1'
 
 outMain='   
+
 int main (int argc, char ** argv)
 {   
+    printf("hello world !\n");
     caml_startup(argv) ;
     return 0 ;
 }'
-mainIncludes="#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>"
+mainIncludes="#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <emscripten.h>"
+source ../emsdk/emsdk_env.sh
+
+cd $HOME/haxec/ocaml #Ocaml 32 bit
+./configure --build=i686-pc-linux-gnu
+ make clean
+ make world.opt
+
+
+emconfigure ./configure
+cd runtime
+emmake make clean
+cd ..
+make runtime
+
+
+#Build external stubs libs
+cd $HOME/haxec/pcre
+emconfigure ./configure
+emmake make clean
+emmake make
+
+cd $HOME/haxec/sha
+emcc -I. -I../ocaml/runtime -I$HOME/.opam/default/lib/ocaml sha1_stubs.c -o libsha1.so
+
+#Build internal stubs libs
+cd $HOME/haxec/ocaml/otherlibs/unix
+emcc -O0 -I../../runtime -I. *.c -o libunix.so
+
+cd $HOME/haxec/ocaml/otherlibs/systhreads
+emcc -I../../runtime -I. st_stubs.c -o libthreads.so
+
+cd $HOME/haxec/ocaml/otherlibs/str
+emcc -I../../runtime  strstubs.c -o libcamstr.a
 
 cur=$HOME/haxe
 cd $HOME/haxe
@@ -28,7 +60,7 @@ rm -f haxe.html
 rm -f haxe.js
 rm -f haxe.wasm
 rm -f haxe.worker.js
-rm -f main.bc
+rm -f main_haxe.c
 
 sed -i -e 's/HAXE_OUTPUT=haxe/HAXE_OUTPUT=haxe.bc.c/g' ./Makefile
 sed -i -e 's/haxe.exe/haxe.bc.c/g' ./Makefile
@@ -56,13 +88,10 @@ for f in $(find $HOME/haxe/libs -name "*.a");
     
 done
 
-cp /usr/include/pcre.h ./
 wasmArgs='-s USE_ZLIB=1 -s FORCE_FILESYSTEM=1'
 
-# emcc $includes $ofiles $linkPaths  -o main.bc  $cargs $wasmArgs  --no-check-feature
-# emcc $includes  -o main.bc $wasmArgs
 sofiles="$HOME/haxec/pcre/.libs/libpcre.so $HOME/haxec/ocaml/runtime/libcamlrun.a $HOME/haxec/ocaml/otherlibs/unix/libunix.so $HOME/haxec/ocaml/otherlibs/systhreads/libthreads.so $HOME/haxec/sha/libsha1.so"
-emcc $includes $sofiles $ofiles main_haxe.c $linkPaths -o haxe.html $wasmArgs -s ERROR_ON_UNDEFINED_SYMBOLS=0
+emcc -g3 $includes $sofiles $ofiles main_haxe.c $linkPaths -o haxe.html $wasmArgs -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=1024MB
 
 echo haxe was built
 
